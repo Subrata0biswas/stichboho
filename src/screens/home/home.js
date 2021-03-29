b13a39c28a2fb99ab74001e72084e715c99a3511
subrata85 <subratabiswas@digitalaptech.co.in>
2021-03-29 11:15:52 +0530

    initial commit

---
+++ src/screens/home/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import connect from 'react-redux'
+
+class Home extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+
+ render(){
+    return(
+        <div>
+            home
+        </div>
+    )
+ }
+}
+
+export default Home;
