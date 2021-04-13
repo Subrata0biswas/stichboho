c25e709d9d88e20888915261c37c9f4b5ce35b52
subrata85 <subratabiswas@digitalaptech.co.in>
2021-04-13 20:38:42 +0530

    new css added

--- src/components/pageNotFound/pageNotFound.js
+++ src/components/pageNotFound/pageNotFound.js
@@ -5,80 +5,22 @@ class PageNotFound extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      code: this.props.code || 404
     }
-  }
-  onClickGoBack() {
-    if (this.props.history) {
-      this.props.history.goBack()
-    }
-  }
-
-  componentWillMount() {
-    if (this.props.code) {
-      this.setState({
-        code: this.props.code
-      })
-    }
-  }
-
-  renderAdmin() {
-    return (
-      <div>
-        <h1> This is for super admin </h1>
-        <p className="zoom-area"> Please login by admin. </p>
-        <section className="error-container">
-          <span>4</span>
-          <span>
-            <span className="screen-reader-text">0</span>
-          </span>
-          <span>1</span>
-        </section>
-        <div className="link-container">
-          <a href="#"
-            className="btn btn-active-link"
-            onClick={() => this.onClickGoBack()}>
-            Go Back
-          </a>
-          <h2>Oops! You Are Not Super Admin.</h2>
-        </div>
-      </div>
-    )
-  }
-
-  renderNotFound() {
-    return (
-      <div>
-        {this.props.code === '501' ? (
-          <h1>You have no permission. Please contact system admin</h1>
-        ) : (
-          <h1> Page Not Found </h1>
-        )}
-        <p className="zoom-area"> Sorry </p>
-        <section className="error-container">
-          {this.props.code === '501' ? <span>5</span> : <span>4</span>}
-          <span>
-            <span className="screen-reader-text">0</span>
-          </span>
-          {this.props.code === '501' ? <span>1</span> : <span>4</span>}
-        </section>
-        <div className="link-container">
-          <a href="#"
-            className="btn btn-active-link"
-            onClick={() => this.onClickGoBack()}>
-            Go Back
-          </a>
-          <h2>Oops! Looks like you landed in a wrong place</h2>
-        </div>
-      </div>
-    )
   }
 
   render() {
     return (
-      <div>
-        {this.state.code === '401' ? this.renderAdmin() : this.renderNotFound()}
-      </div>
+      <main>
+      <div class="main-border">
+        <div class="eror-page cms-con">
+            <div class="eror-outer">
+              <h2>404</h2>
+              <p>The link you clicked may be broken or the page may have been removed</p>
+              <p>Visit the <a href="#">Home Page</a> Or <a  href="#">Contact</a> me</p>
+            </div>
+        </div>
+        </div>	
+    </main>
     )
   }
 }
