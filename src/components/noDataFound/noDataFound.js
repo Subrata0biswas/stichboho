93f6d715c3104f47798d3b7dbaaf5e85bcfb98de
subrata85 <subratabiswas@digitalaptech.co.in>
2021-05-01 18:41:37 +0530

    no data found wip

---
+++ src/components/noDataFound/noDataFound.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function NoDataFound(props) {
+  return (
+    <main>
+      <div class="main-border">
+        <div class="eror-page cms-con">
+          <div class="eror-outer no-data">
+            <h2>
+              Oops! <br />
+              <span>NO DATA FOUND</span>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+export default NoDataFound;
